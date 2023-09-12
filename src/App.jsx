01b3a577bd6c3e24d4c1b6fc0fcf85f4fe777f71
ddmuzyk <domino2k1@gmail.com>
2023-09-12 22:36:01 +0200

    adjusting styles for the whole document

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='home-section'></div>
+    <section className='home-section'>
+      <div className='content'>
+        <h2 >Join 2 million+ Australians finding better</h2>
+        <p>Finder's team of 40+ experts will help you find the right choices in over 100 categories.</p>
+      </div>
+      <div className='tiles-container'>
+        <div className='tiles'>
+          
+          <div></div>
+        </div>
+      </div>
+    </section>
   )
 }
 
