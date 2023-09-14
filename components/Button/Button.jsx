12c816e692aa6989e4bf5c9992ef78d805ad093b
ddmuzyk <domino2k1@gmail.com>
2023-09-14 22:17:53 +0200

    created the tile expanding functionality

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Button.css';
 
-const Button = ({isOpen}) => {
+const Button = ({isOpen, setIsOpen}) => {
   return (
-    <div className='button-container'>
+    <div onClick={() => {
+      setIsOpen(!isOpen);
+    }} className='button-container'>
       <button className="expand-button">
-        <span>Show more categories</span>
+        <span>Show {isOpen ? 'less' : 'more'} categories</span>
       </button>
         <div className="arrow-container">
           <img className="arrow" src={`../../public/${isOpen ? 'up' : 'down'}.svg`}></img>
--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,17 @@ function App() {
       <div className='content'>
         <h2 >Join 2 million+ Australians finding better</h2>
         <p>Finder's team of 40+ experts will help you find the right choices in over 100 categories.</p>
-      <div className='tiles-container'>
-        <div className='tiles'>
-          {data.map((item, index) => {
-            return (
-              <Tile key={index} title={item.title} image={item.image} />
-            )
-          })}
+        <div className='tiles-container'>
+          <div className={`tiles ${isOpen ? 'open' : ''}`}>
+            {data.map((item, index) => {
+              return (
+                <Tile key={index} title={item.title} image={item.image} />
+              )
+            })}
+          </div>
+          <Button isOpen={isOpen} setIsOpen={setIsOpen}
+          />
         </div>
-        <Button isOpen={isOpen}/>
-      </div>
       </div>
     </section>
   )
